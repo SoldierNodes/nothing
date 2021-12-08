adbb385c34ad95bfabf28b79394cd465ee944298
AmeliaDGrund <AmeliaDGrund@protonmail.com>
2021-12-08 23:05:27 +0100

    Parfait

--- pages/armies.vue
+++ pages/armies.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="mt-16 flex flex-col items-center">
     <div class="font-ops text-4xl uppercase">Your army</div>
+    <div class="mt-10" />
+    <div
+      class="w-full md:w-3/4 lg:w-5/6 xl:w-3/4 grid grid-cols-2 gap-6 text-xl font-ops uppercase"
+    >
+      <div
+        class="bg-army bg-opacity-70 h-32 rounded-lg flex items-center justify-center text-white"
+      >
+        Your Army: 1
+      </div>
+      <div
+        class="bg-army bg-opacity-70 h-32 rounded-lg flex items-center justify-center text-white"
+      >
+        Your soldiers: 12
+      </div>
+      <div
+        class="bg-gray-300 h-32 rounded-lg flex items-center justify-center col-span-2"
+      >
+        Comming Soon...
+      </div>
+    </div>
   </div>
 </template>
