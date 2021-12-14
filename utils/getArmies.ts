e0652c571bd3a067a3f387a49d5122e367a09776
BossSuricate <suricategamingytb@gmail.com>
2021-12-15 00:22:36 +0100

    Tg

--- utils/getArmies.ts
+++ utils/getArmies.ts
@@ -7,6 +7,12 @@ const oldmanager = '0x125AB7BA207824ECE40722070370eC5A38bBfa89'
 
 const abi = [
   'function getNodesStringOf(address _account) external view returns (string memory)',
+  'function balanceOf(address owner) public override view returns (uint256 balance)',
+  'function getNodesIdsOf(address _account) public view returns (uint64[] memory)',
+  'function getNameOf(uint64 _id) public view returns (string memory)',
+  'function getMintOf(uint64 _id) public view returns (uint64)',
+  'function getClaimOf(uint64 _id) public view returns (uint64)',
+  'function getEarnedOf(uint64 _id) public view returns (uint64)',
 ]
 
 const abiHelper = [
@@ -25,49 +31,107 @@ export default async (
 ) => {
   const contract = new ethers.Contract(manager, abi, provider)
 
-  const contractXD = new ethers.Contract(helper, abiHelper, provider)
-  const armies: String = await contract.getNodesStringOf(account)
-  const possibleMint = (
-    (await contractXD.notMigrated(account)) as BigNumber
-  ).toNumber()
+  const balanceof = contract.balanceOf(account)
 
-  let realArmies: any = []
+  if (balanceof.gt(BigNumber.from(200))) {
+    const contractXD = new ethers.Contract(helper, abiHelper, provider)
+    const armiesId: Array<BigNumber> = await contract.getNodesIdsOf(account)
 
-  if (armies !== '') {
-    realArmies = armies.split(`#`).map((value: String) => {
-      const values = value.split(`.`)
-      return {
-        id: values[0],
-        name: values[1],
-        mint: values[2],
-        claim: values[3],
-        earned: values[4],
-      }
-    })
-  }
+    const realArmies: any = []
 
-  const oldContract = new ethers.Contract(oldmanager, oldabi, provider)
+    for (let i = 0; i < armiesId.length; i++) {
+      const id = armiesId[i]
 
-  const notMigrated = []
+      const promises = [
+        contract.getNameOf(id),
+        contract.getMintOf(id),
+        contract.getClaimOf(id),
+        contract.getEarnedOf(id),
+      ]
 
-  try {
-    const names = await oldContract._getNodesNames(account)
-    const namesArray = names.split('#')
-    const claims = await oldContract._getNodesLastClaimTime(account)
-    const claimsArray = claims.split('#')
-    const creation = await oldContract._getNodesCreationTime(account)
-    const creationArray = creation.split('#')
+      const [name, mint, claim, earned] = await Promise.all(promises)
 
-    for (let i = 0; i < possibleMint; i++) {
-      notMigrated.push({
-        id: -1,
-        name: namesArray[i],
-        mint: creationArray[i],
-        claim: claimsArray[i],
-        earned: 2,
+      realArmies.push({
+        id: id.toString(),
+        name,
+        mint: parseInt(mint),
+        claim: parseInt(claim),
+        earned,
       })
     }
-  } catch (error) {}
+    const possibleMint = (
+      (await contractXD.notMigrated(account)) as BigNumber
+    ).toNumber()
 
-  return [...notMigrated, ...realArmies]
+    const oldContract = new ethers.Contract(oldmanager, oldabi, provider)
+
+    const notMigrated = []
+
+    try {
+      const names = await oldContract._getNodesNames(account)
+      const namesArray = names.split('#')
+      const claims = await oldContract._getNodesLastClaimTime(account)
+      const claimsArray = claims.split('#')
+      const creation = await oldContract._getNodesCreationTime(account)
+      const creationArray = creation.split('#')
+
+      for (let i = 0; i < possibleMint; i++) {
+        notMigrated.push({
+          id: -1,
+          name: namesArray[i],
+          mint: creationArray[i],
+          claim: claimsArray[i],
+          earned: 2,
+        })
+      }
+    } catch (error) {}
+
+    return [...notMigrated, ...realArmies]
+  } else {
+    const contractXD = new ethers.Contract(helper, abiHelper, provider)
+    const armies: String = await contract.getNodesStringOf(account)
+    const possibleMint = (
+      (await contractXD.notMigrated(account)) as BigNumber
+    ).toNumber()
+
+    let realArmies: any = []
+
+    if (armies !== '') {
+      realArmies = armies.split(`#`).map((value: String) => {
+        const values = value.split(`.`)
+        return {
+          id: values[0],
+          name: values[1],
+          mint: values[2],
+          claim: values[3],
+          earned: values[4],
+        }
+      })
+    }
+
+    const oldContract = new ethers.Contract(oldmanager, oldabi, provider)
+
+    const notMigrated = []
+
+    try {
+      const names = await oldContract._getNodesNames(account)
+      const namesArray = names.split('#')
+      const claims = await oldContract._getNodesLastClaimTime(account)
+      const claimsArray = claims.split('#')
+      const creation = await oldContract._getNodesCreationTime(account)
+      const creationArray = creation.split('#')
+
+      for (let i = 0; i < possibleMint; i++) {
+        notMigrated.push({
+          id: -1,
+          name: namesArray[i],
+          mint: creationArray[i],
+          claim: claimsArray[i],
+          earned: 2,
+        })
+      }
+    } catch (error) {}
+
+    return [...notMigrated, ...realArmies]
+  }
 }
